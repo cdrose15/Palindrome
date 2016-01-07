762d7d1ce9b9aa9b58e483d50e505d6537283921
Chris Rose <cdrose15@gmail.com>
2016-01-06 23:17:13 -0500

    Palindrome
    1st upload

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $("#palindrome").click(function () {
+
+        var string = $("#word").val();
+        string = string.toUpperCase();
+        string = string.trim();
+        var reverseString = string.split('').reverse().join('');
+
+        if (string == reverseString) {
+            $("#result").append("Yep! " + string + " is a Palindrome");
+        }
+        else {
+            $("#result").append("Sorry! " + string + " is not a Palindrome");
+        }
+    });
+});
+
